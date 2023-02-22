dc7e1d297e252a65e3ac0b3b2bbc1db69a96220d
Steven <52871164+sstonaker@users.noreply.github.com>
2023-02-21 18:25:24 -0600

    advanced react

---
+++ advanced-react/week4/src/App.js
@@ -0,0 +1,27 @@
+import { ChakraProvider } from "@chakra-ui/react";
+import Alert from "./components/Alert";
+import ContactMeSection from "./components/ContactMeSection";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import LandingSection from "./components/LandingSection";
+import ProjectsSection from "./components/ProjectsSection";
+import { AlertProvider } from "./context/alertContext";
+
+function App() {
+  return (
+    <ChakraProvider>
+      <AlertProvider>
+        <main>
+          <Header />
+          <LandingSection />
+          <ProjectsSection />
+          <ContactMeSection />
+          <Footer />
+          <Alert />
+        </main>
+      </AlertProvider>
+    </ChakraProvider>
+  );
+}
+
+export default App;
